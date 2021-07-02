77117456c96b6fd09b195e879e39f7ea49a2c294
shinyay <shinya.com@gmail.com>
2021-07-03 07:54:36 +0900

    Add GetMapping

--- src/main/kotlin/com/google/shinyay/controller/BookController.kt
+++ src/main/kotlin/com/google/shinyay/controller/BookController.kt
@@ -1,9 +1,14 @@
 package com.google.shinyay.controller
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1")
 class BookController {
+
+    @GetMapping
+    fun info() = "Spring Data JPA with Kotlin and H2"
+    
 }
