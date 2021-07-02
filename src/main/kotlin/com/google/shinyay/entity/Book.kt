bf1036663d698431521962759aea4bb94c6888fa
shinyay <shinya.com@gmail.com>
2021-07-02 22:24:59 +0900

    Add Book entity

---
+++ src/main/kotlin/com/google/shinyay/entity/Book.kt
@@ -0,0 +1,13 @@
+package com.google.shinyay.entity
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+data class Book(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long
+)
