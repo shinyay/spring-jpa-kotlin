3fddce2877bbc4bf637500ca8ce2330b83ddd0ae
shinyay <shinya.com@gmail.com>
2021-07-03 22:43:29 +0900

    Add update book information function

--- src/main/kotlin/com/google/shinyay/service/BookService.kt
+++ src/main/kotlin/com/google/shinyay/service/BookService.kt
@@ -1,9 +1,13 @@
 package com.google.shinyay.service
 
+import com.google.shinyay.entity.Book
 import com.google.shinyay.repository.BookRepository
 import org.springframework.stereotype.Service
 
 @Service
 class BookService(val repository: BookRepository) {
 
+    fun updateBookInformation(book: Book) {
+        
+    }
 }
