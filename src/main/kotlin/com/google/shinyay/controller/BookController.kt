d7da1f76dce076374642d5519f438e1771a529af
shinyay <shinya.com@gmail.com>
2021-07-03 22:29:40 +0900

    Add save a book with POST

--- src/main/kotlin/com/google/shinyay/controller/BookController.kt
+++ src/main/kotlin/com/google/shinyay/controller/BookController.kt
@@ -4,10 +4,7 @@ import com.google.shinyay.entity.Book
 import com.google.shinyay.repository.BookRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
@@ -33,5 +30,10 @@ class BookController(val repository: BookRepository) {
             return ResponseEntity(HttpStatus.NOT_FOUND)
         }
         return ResponseEntity(book, HttpStatus.OK)
+    }
+
+    @PostMapping("/books")
+    fun addNewBook(@RequestBody book: Book): ResponseEntity<Book> {
+        return ResponseEntity(repository.save(book), HttpStatus.OK)
     }
 }
