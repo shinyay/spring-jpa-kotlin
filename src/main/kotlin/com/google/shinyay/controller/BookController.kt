6eea152cfc5674b8213daa0a5d2e4f19b9962869
shinyay <shinya.com@gmail.com>
2021-07-03 22:16:18 +0900

    Add find one book by ID with GET

--- src/main/kotlin/com/google/shinyay/controller/BookController.kt
+++ src/main/kotlin/com/google/shinyay/controller/BookController.kt
@@ -5,8 +5,10 @@ import com.google.shinyay.repository.BookRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("/api/v1")
@@ -22,5 +24,14 @@ class BookController(val repository: BookRepository) {
             return ResponseEntity(HttpStatus.NO_CONTENT)
         }
         return ResponseEntity(books, HttpStatus.OK)
+    }
+
+    @GetMapping("/books/{id}")
+    fun findOneBookById(@PathVariable("id") id: Long): ResponseEntity<Optional<Book>> {
+        val book = repository.findById(id)
+        if (book.isEmpty) {
+            return ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+        return ResponseEntity(book, HttpStatus.OK)
     }
 }
