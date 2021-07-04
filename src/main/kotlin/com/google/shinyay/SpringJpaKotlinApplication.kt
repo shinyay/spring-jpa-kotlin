bc083f654a7f8094344d35af72ff77b7aeee92dc
shinyay <shinya.com@gmail.com>
2021-07-05 08:36:54 +0900

    Add contance information

--- src/main/kotlin/com/google/shinyay/SpringJpaKotlinApplication.kt
+++ src/main/kotlin/com/google/shinyay/SpringJpaKotlinApplication.kt
@@ -1,6 +1,7 @@
 package com.google.shinyay
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
 import io.swagger.v3.oas.annotations.info.Info
 import io.swagger.v3.oas.annotations.info.License
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -12,7 +13,8 @@ import org.springframework.boot.runApplication
 		title = "Books API",
 		description = "Spring Data JPA + H2",
 		version = "v1",
-		license = License(name = "MIT license", url = "https://bit.ly/2Tu1lFg" )
+		license = License(name = "MIT license", url = "https://bit.ly/2Tu1lFg" ),
+		contact = Contact(name = "shinyay", url = "https://twitter.com/yanashin18618")
 	)
 )
 class SpringJpaKotlinApplication
