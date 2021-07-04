3c20458dc9a6df6508a52ff51f092bb18bce405f
shinyay <shinya.com@gmail.com>
2021-07-05 06:11:46 +0900

    Add OpenApiDefinition

--- src/main/kotlin/com/google/shinyay/SpringJpaKotlinApplication.kt
+++ src/main/kotlin/com/google/shinyay/SpringJpaKotlinApplication.kt
@@ -1,9 +1,18 @@
 package com.google.shinyay
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = Info(
+		title = "Books API",
+		description = "Spring Data JPA + H2",
+		version = "v1"
+	)
+)
 class SpringJpaKotlinApplication
 
 fun main(args: Array<String>) {
