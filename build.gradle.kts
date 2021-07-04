6eb604892ed499afbcf81435ff199b8027923958
shinyay <shinya.com@gmail.com>
2021-07-05 06:41:28 +0900

    Add OpenApi for Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springdoc:springdoc-openapi-ui:1.5.9")
+	implementation("org.springdoc:springdoc-openapi-kotlin:1.5.9")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
